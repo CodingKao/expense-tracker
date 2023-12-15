dfab13447d106f63de30826bb4467637e6756851
Kao Saelor <saelor.kao@gmail.com>
2023-12-15 11:06:50 -0800

    trying to connect mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require('express')
 const cors = require('cors');
 const { db } = require('./db/db');
-const app = express();
+const app = express()
 
 require('dotenv').config();
 
