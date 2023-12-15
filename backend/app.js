0df68e6386c1370d0f62389460bc79c6797e09af
Kao Saelor <saelor.kao@gmail.com>
2023-12-15 00:11:42 -0800

    thunderclient proof of life

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,24 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-require('dotenv').config()
+require('dotenv').config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 // middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
 
 const server = () => {
-    console.log('You are listening on port:', PORT);
-}
+    app.listen(PORT, () => {
+        console.log('You are listening on port:', PORT);
+    });
+};
 
 server();
